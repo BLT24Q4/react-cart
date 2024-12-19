6ad69c6fb352d7ab779a161b95911ee67939a8c4
Sean Nam <blt24q4@gmail.com>
2024-12-19 12:01:45 +0900

    전체 목록, 구매 목록 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import CartHeader from "./components/CartHeader";
 import ShopList from "./components/ShopList";
 import CartInput from "./components/CartInput";
@@ -5,17 +7,51 @@ import BoughtList from "./components/BoughtList";
 import CartFooter from "./components/CartFooter";
 
 function App() {
+  const [itemList, setItemList] = useState([
+    { id: 1, name: "무", isBought: false },
+    { id: 2, name: "배추", isBought: false },
+    { id: 3, name: "쪽파", isBought: true },
+    { id: 4, name: "고춧가루", isBought: false },
+  ]);
+
+  //  isBought === false인 것만 필터링
+  //  isBought === false인 것들
+  const shopItems = itemList.filter((item) => !item.isBought);
+
+  //  id => isBought를 true <-> false
+  const toggleBought = (id) => {
+    const newItemList = itemList.map((item) =>
+      item.id === id ? { ...item, isBought: !item.isBought } : item
+    );
+    setItemList(newItemList);
+  };
+
+  //  id => item 삭제
+  const deleteItem = (id) => {
+    const newItemList = itemList.filter((item) => item.id !== id);
+    setItemList(newItemList);
+  };
+
   return (
     <div>
       <CartHeader />
       <main>
         <section>
           <h2>전체 목록</h2>
+          <ul>
+            {itemList.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
         </section>
-        <ShopList />
+        <ShopList
+          items={shopItems}
+          toggleBought={toggleBought}
+          deleteItem={deleteItem}
+        />
 
         <CartInput />
-        <input type="checkbox" id="show-bought-items" />
+        <input type="checkbox" id="show-bought-items" checked={true} />
         <label>산 물건 보기</label>
         <BoughtList />
       </main>
