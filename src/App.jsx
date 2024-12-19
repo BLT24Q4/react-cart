6fb0a9192fdee118efcebbad30034e20d7631c9d
Sean Nam <blt24q4@gmail.com>
2024-12-19 10:30:49 +0900

    Split App to Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import CartHeader from "./components/CartHeader";
+import ShopList from "./components/ShopList";
+import CartInput from "./components/CartInput";
+import BoughtList from "./components/BoughtList";
+import CartFooter from "./components/CartFooter";
 
 function App() {
-  return <div>React Cart</div>;
+  return (
+    <div>
+      <CartHeader />
+      <main>
+        <section>
+          <h2>전체 목록</h2>
+        </section>
+        <ShopList />
+
+        <CartInput />
+        <input type="checkbox" id="show-bought-items" />
+        <label>산 물건 보기</label>
+        <BoughtList />
+      </main>
+      <CartFooter />
+    </div>
+  );
 }
 
 export default App;
